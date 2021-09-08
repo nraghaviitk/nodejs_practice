e5b121001928b64f9855ee2c8874f50e187ada08
raghav <raghav@debian.raghav>
2021-09-08 19:48:37 +0530

    first commit

--- myfirst.js
+++ myfirst.js
@@ -4,5 +4,6 @@ var dt   = require('./mymodule.js');
 http.createServer(function(req,res){
 	res.writeHead(200,{'Content-Type': 'text/html'});
 	res.write("todays date and time"+ dt.myDateTime());
+
 	res.end('Hello World');
 }).listen(8080);
